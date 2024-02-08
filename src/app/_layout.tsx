15d47b9b22b3a0b99eedfc8c100a5e9e244f546d
Vasu_Bhanderi <vbbhanderi1312@gmail.com>
2024-02-08 13:33:16 +0530

    Modal animations update

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -57,7 +57,10 @@ function RootLayoutNav() {
       <CartProvider>
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="cart" options={{ presentation: "modal" }} />
+          <Stack.Screen
+            name="cart"
+            options={{ presentation: "modal", animation: "slide_from_bottom", title: "Cart"}}
+          />
         </Stack>
       </CartProvider>
     </ThemeProvider>
