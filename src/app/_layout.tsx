e9d04e16b1af8d7c9d2f10ace4f7170b1cb9484f
Vasu_Bhanderi <vbbhanderi1312@gmail.com>
2024-02-09 01:25:00 +0530

    Basic Auth Flow

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -11,6 +11,7 @@ import { useEffect } from "react";
 
 import { useColorScheme } from "../components/useColorScheme";
 import CartProvider from "@/providers/CartProvider";
+import AuthProvider from "@/providers/AuthProvider";
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -54,21 +55,24 @@ function RootLayoutNav() {
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <CartProvider>
-        <Stack>
-          <Stack.Screen name="(user)" options={{ headerShown: false }} />
-          <Stack.Screen name="(admin)" options={{ headerShown: false }} />
-          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-          <Stack.Screen
-            name="cart"
-            options={{
-              presentation: "modal",
-              animation: "slide_from_bottom",
-              title: "Cart",
-            }}
-          />
-        </Stack>
-      </CartProvider>
+      <AuthProvider>
+        <CartProvider>
+          <Stack>
+            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+            <Stack.Screen name="(user)" options={{ headerShown: false }} />
+            <Stack.Screen name="(admin)" options={{ headerShown: false }} />
+
+            <Stack.Screen
+              name="cart"
+              options={{
+                presentation: "modal",
+                animation: "slide_from_bottom",
+                title: "Cart",
+              }}
+            />
+          </Stack>
+        </CartProvider>
+      </AuthProvider>
     </ThemeProvider>
   );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,21 @@
 import { View, Text } from "react-native";
 import React from "react";
 import Button from "../components/Button";
-import { Link } from "expo-router";
+import { Link, Redirect } from "expo-router";
+import { useAuth } from "@/providers/AuthProvider";
+import { ActivityIndicator } from "react-native";
+import { supabase } from "@/lib/supabase";
 
 const index = () => {
+  const { session, loading } = useAuth();
+
+  if (loading) {
+    return <ActivityIndicator />;
+  }
+
+  if (!session) {
+    return <Redirect href={"/sign-in"} />;
+  }
   return (
     <View style={{ flex: 1, justifyContent: "center", padding: 10 }}>
       <Link href={"/(user)"} asChild>
@@ -15,6 +27,7 @@ const index = () => {
       <Link href={"/(auth)/sign-in"} asChild>
         <Button text="Sign In" />
       </Link>
+      <Button text="Sign Out" onPress={() => supabase.auth.signOut()} />
     </View>
   );
 };
