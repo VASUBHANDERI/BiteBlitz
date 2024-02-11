7275c5c3c197eff04379e8da4de947e98924866f
Vasu_Bhanderi <vbbhanderi1312@gmail.com>
2024-02-11 17:30:01 +0530

    Size based pricing implemented

--- src/components/OrderItemListItem.tsx
+++ src/components/OrderItemListItem.tsx
@@ -1,7 +1,7 @@
 import { View, Text, StyleSheet, Image } from "react-native";
 import React from "react";
 import Colors from "../constants/Colors";
-import { OrderItem, Tables } from "../types";
+import { OrderItem, PizzaSize, Tables } from "../types";
 import { defaultImage } from "./ProductListItem";
 import RemoteImage from "./RemoteImage";
 
@@ -10,25 +10,43 @@ type OrderItemListItemProps = {
 };
 
 const OrderItemListItem = ({ item }: OrderItemListItemProps) => {
-  return (
-    <View style={styles.container}>
-      <RemoteImage
-        path={item.products?.image}
-        fallback={defaultImage}
-        style={{ ...styles.image, resizeMode: "contain" }}
-      />
-      <View style={{ flex: 1 }}>
-        <Text style={styles.title}>{item.products?.name}</Text>
-        <View style={styles.subtitleContainer}>
-          <Text style={styles.price}>${item.products?.price.toFixed(2)}</Text>
-          <Text>Size: {item.size}</Text>
+  const getFactor = (size: string) => {
+    if (size == "M") {
+      return 1;
+    } else if (size == "L") {
+      return 1.5;
+    } else if (size == "XL") {
+      return 2;
+    } else if (size == "S") {
+      return 0.8;
+    }
+    return 1;
+  };
+  if(item.products){
+    const finalPrice =
+      item.products?.price * getFactor(item.size);
+    const finalTotal = finalPrice * item.quantity;
+    return (
+      <View style={styles.container}>
+        <RemoteImage
+          path={item.products?.image}
+          fallback={defaultImage}
+          style={{ ...styles.image, resizeMode: "contain" }}
+        />
+        <View style={{ flex: 1 }}>
+          <Text style={styles.title}>{item.products?.name}</Text>
+          <View style={styles.subtitleContainer}>
+            <Text style={styles.price}>${finalPrice.toFixed(2)}</Text>
+            <Text>Size: {item.size}</Text>
+          </View>
+        </View>
+        <View style={styles.quantitySelector}>
+          <Text style={styles.quantity}>{item.quantity}</Text>
+          <Text style={styles.finalTotal}>{finalTotal}</Text>
         </View>
       </View>
-      <View style={styles.quantitySelector}>
-        <Text style={styles.quantity}>{item.quantity}</Text>
-      </View>
-    </View>
-  );
+    );
+  }
 };
 
 const styles = StyleSheet.create({
@@ -56,14 +74,20 @@ const styles = StyleSheet.create({
     gap: 5,
   },
   quantitySelector: {
-    flexDirection: "row",
-    gap: 10,
+    // flexDirection: "row",
+    // gap: 10,
     alignItems: "center",
-    marginVertical: 10,
+    // marginVertical: 10,
   },
   quantity: {
     fontWeight: "500",
-    fontSize: 18,
+    fontSize: 16,
+    alignSelf: "flex-end",
+  },
+  finalTotal: {
+    fontWeight: "500",
+    fontSize: 16,
+    color: Colors.light.tint,
   },
   price: {
     color: Colors.light.tint,
